e66b13a772bd01eadec38ab26853dec5a0a3a9ef
Hector Montero <E1003942@ercbpo.com>
2019-11-22 20:39:24 -0400

    Add documentation to Shuffle extension methods

--- DWC.Blazor/Extensions/LinqExtensions.cs
+++ DWC.Blazor/Extensions/LinqExtensions.cs
@@ -6,11 +6,20 @@ namespace DWC.Blazor.Extensions
 {
     public static class LinqExtensions
     {
+        /// <summary>Returns a shuffled sequence from the original sequence.</summary>
+        /// <typeparam name="T">The type of the elements of <paramref name="source"/></typeparam>
+        /// <param name="source">The sequence to shuffle.</param>
+        /// <returns>A new shuffled sequence of <paramref name="source"/></returns>
         public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source)
         {
             return Shuffle(source, new Random());
         }
 
+        /// <summary>Returns a shuffled sequence from the original sequence.</summary>
+        /// <typeparam name="T">The type of the elements of <paramref name="source"/></typeparam>
+        /// <param name="source">The sequence to shuffle.</param>
+        /// <param name="rand">A random generator used as part of the selection algorithm.</param>
+        /// <returns>A new shuffled sequence of <paramref name="source"/></returns>
         public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source, Random rand)
         {
             if (source == null) throw new ArgumentNullException(nameof(source));
