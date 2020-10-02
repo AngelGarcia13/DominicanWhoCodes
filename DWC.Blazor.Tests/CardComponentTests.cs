cf4466cb7b32c6f889be38dba0b414ea036aebf0
nminaya <nelsonminayaperez@gmail.com>
2020-10-01 21:35:19 -0400

    Add SocialNetworks_AllSocialNetworkFilled_Should_DisplaysAllSocilNetworks

--- DWC.Blazor.Tests/CardComponentTests.cs
+++ DWC.Blazor.Tests/CardComponentTests.cs
@@ -35,5 +35,48 @@ namespace DWC.Blazor.Tests
             // Assert
             Assert.Throws<ElementNotFoundException>(() => component.Find("div .social-networks .fa-linkedin"));
         }
+
+        [Fact]
+        public void SocialNetworks_AllSocialNetworkFilled_Should_DisplaysAllSocilNetworks()
+        {
+            // Arrange
+            using var context = new TestContext();
+
+            var parameters = new ComponentParameter[]
+            {
+                ("Name", "Test Name"),
+                ("Initials", "TN"),
+                ("Image", "image.jpg"),
+                ("Summary", "Test Summary"),
+                ("Webpage", "http://mypage.com"),
+                ("Twitter", "http://twitter.com"),
+                ("Skills","C#, ASP.NET Core, T-SQL, TypeScript, JavaScrpit"),
+                ("Twitter", "http://twitter.com"),
+                ("Telegram", "http://telegram.com"),
+                ("StackOverflow", "http://stackoverflow.com"),
+                ("Medium", "http://medium.com"),
+                ("Github", "http://github.com"),
+                ("YouTube", "http://youtube.com"),
+                ("LinkedIn", "http://linkedin.com")
+            };
+
+            // Act
+            var component = context.RenderComponent<CardComponent>(parameters);
+
+            // Assert
+            var socialNetworkDiv = component.Find("div .social-networks");
+
+            Assert.True(socialNetworkDiv.HasChildNodes);
+            Assert.Equal(8, socialNetworkDiv.ChildElementCount); // There should be 8 elements (Social Networks)
+
+            Assert.NotNull(component.Find("div .social-networks .fa-globe-americas"));
+            Assert.NotNull(component.Find("div .social-networks .fa-linkedin"));
+            Assert.NotNull(component.Find("div .social-networks .fa-twitter"));
+            Assert.NotNull(component.Find("div .social-networks .fa-github"));
+            Assert.NotNull(component.Find("div .social-networks .fa-paper-plane"));
+            Assert.NotNull(component.Find("div .social-networks .fa-stack-overflow"));
+            Assert.NotNull(component.Find("div .social-networks .fa-medium"));
+            Assert.NotNull(component.Find("div .social-networks .fa-youtube"));
+        }
     }
 }
