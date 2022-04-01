5270a9032453395bc912c4c302a5155b0d0316b4
Jadhiel Vélez <jadhielv@gmail.com>
2022-04-01 19:25:10 -0400

    Cosmetic patch

--- DWC.Blazor/Program.cs
+++ DWC.Blazor/Program.cs
@@ -11,8 +11,8 @@ namespace DWC.Blazor
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
 
+            builder.RootComponents.Add<App>("#app");
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
